93e41717877b253eb17f9532930c4c7e009ba581
subhankhalid402 <subhan.khalid402@gmail.com>
2024-07-02 13:17:04 +0500

    Update forminfo.blade.php

--- resources/views/pdf/forminfo.blade.php
+++ resources/views/pdf/forminfo.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -25,23 +26,21 @@
             margin: 0;
             padding: 0;
             background-color: #f4f4f9;
-            font-family: 'Helvetica';
-            font-size: 12px;
+            font-family: 'Helvetica', sans-serif;
+            font-size: 15px;
             color: #333;
         }
 
         .page {
-            width: 190mm;
-            margin: 10mm auto;
-            margin-top: 7px;
-            padding: 20mm;
+            /* width: 210mm; */
+            margin: 0 auto;
             background-color: #fff;
             border-radius: 8px;
             /* box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); */
         }
 
         .subpage {
-            /* padding: 10mm; */
+            padding: 8mm;
         }
 
         @page {
@@ -49,148 +48,199 @@
             footer: page-footer;
         }
 
-        .form-group {
-            display: flex;
-            flex-wrap: wrap;
-            margin-bottom: 15px;
-        }
-
-        .form-group label {
-            flex: 1 0 30%;
-            font-weight: bold;
-            margin-bottom: 5px;
-            color: #555;
-        }
-
-        .form-group p {
-            flex: 1 0 70%;
-            padding: 8px;
-            font-size: 12pt;
-            background-color: #eaeef1;
-            border: 1px solid #ccc;
-            border-radius: 4px;
-            color: #555;
-            margin: 0;
-        }
-
-        .form-row {
-            display: flex;
-            justify-content: space-between;
-            margin-bottom: 15px;
-        }
-
-        .form-column {
-            flex: 0 0 48%;
-        }
-
-        .form-group ul {
-            list-style-type: none;
-            padding: 0;
-            margin: 0;
-        }
-
-        .form-group ul li {
-            background-color: #eaeef1;
-            padding: 8px;
-            margin-bottom: 5px;
-            border: 1px solid #ccc;
-            border-radius: 4px;
-            color: #555;
-        }
-
-        .text-center {
+        .header {
             text-align: center;
-        }
-
-        .contract-title {
-            font-size: 24pt;
-            border-bottom: 3px solid #9c27b0;
-            margin-top: 30px;
             margin-bottom: 30px;
-            text-align: center;
-            color: #9c27b0;
+        }
+
+        .header h1 {
+            font-size: 32pt;
+            margin: 0;
+            color: #e15b64;
+        }
+
+        .header p {
+            font-size: 16pt;
+            color: #666;
+        }
+
+        table {
+            width: 100%;
+        }
+
+        td {
+            padding: 8px;
+        }
+
+        th,
+        td {
+            text-align: left;
+        }
+
+        th {
+            background-color: #f2f2f2;
+            /* font-weight: bold; */
+        }
+
+        .footer {
+            text-align: right;
+            margin-top: 50px;
+        }
+
+        .footer p {
+            margin: 0;
+            font-size: 12pt;
+            color: #555;
+        }
+
+        .info {
+            /* border: 1px solid #797474; */
+            padding: 8px;
+            font-weight: bold;
+            width: 100% !important;
+            border-radius: 4px;
+            background-color: #f9f9f9;
+        }
+
+        .label {
+            /* font-weight: bold; */
+            color: #555;
+        }
+
+        .data-cell {
+            padding-top: 8px;
         }
     </style>
 </head>
+
 <body>
-<div class="book">
-    <div class="page">
-        <div class="subpage">
-            <h1 class="contract-title">Student Information</h1>
-            <div class="form-row">
-                <div class="form-column">
-                    <div class="form-group">
-                        <label for="name">Name:</label>
-                        <p id="name">{{ $student->name ?? 'N/A' }}</p>
-                    </div>
+    <div class="book">
+        <div class="page">
+            <div class="subpage">
+                <div class="header">
+                    <h1>Registration Form</h1>
                 </div>
-                <div class="form-column">
-                    <div class="form-group">
-                        <label for="dob">Date of Birth:</label>
-                        <p id="dob">{{ $student->dob ?? 'N/A' }}</p>
-                    </div>
-                </div>
-            </div>
-            <div class="form-row">
-                <div class="form-column">
-                    <div class="form-group">
-                        <label for="phone">Phone Number:</label>
-                        <p id="phone">{{ $student->phone ?? 'N/A' }}</p>
-                    </div>
-                </div>
-                <div class="form-column">
-                    <div class="form-group">
-                        <label for="email">Email:</label>
-                        <p id="email">{{ $student->email ?? 'N/A' }}</p>
-                    </div>
-                </div>
-            </div>
-            <div class="form-row">
-                <div class="form-column">
-                    <div class="form-group">
-                        <label for="address">Address:</label>
-                        <p id="address">{{ $student->address ?? 'N/A' }}</p>
-                    </div>
-                </div>
-                <div class="form-column">
-                    <div class="form-group">
-                        <label for="cnic">CNIC:</label>
-                        <p id="cnic">{{ $student->cnic ?? 'N/A' }}</p>
-                    </div>
-                </div>
-            </div>
-            <div class="form-row">
-                <div class="form-column">
-                    <div class="form-group">
-                        <label for="education">Education:</label>
-                        <p id="education">{{ $student->education ?? 'N/A' }}</p>
-                    </div>
-                </div>
-                <div class="form-column">
-                    <div class="form-group">
-                        <label for="courses">Courses:</label>
-                        <ul>
-                            <li>{{ $course[0]->title ?? 'N/A' }}</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <div class="form-row">
-                <div class="form-column">
-                    <div class="form-group">
-                        <label for="admission_date">Admission Date:</label>
-                        <p id="admission_date">{{ $student->admission_date ?? 'N/A' }}</p>
-                    </div>
-                </div>
-                <div class="form-column">
-                    <div class="form-group">
-                        <label for="batch">Batch:</label>
-                        <p id="batch">{{ $student->batch->title ?? 'N/A' }}</p>
-                    </div>
+                <table>
+                    <tr>
+                        <td>
+                            <table>
+                                <tr>
+                                    <td class="label">Name:</td>
+                                </tr>
+                                <tr>
+                                    <td class="info">{{ $student->name ?? 'N/A' }}</td>
+                                </tr>
+                            </table>
+                        </td>
+                        <td>
+                            <table>
+                                <tr>
+                                    <td class="label">Date of Birth:</td>
+                                </tr>
+                                <tr>
+                                    <td class="info">{{ $student->dob ?? 'N/A' }}</td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <table>
+                                <tr>
+                                    <td class="label">Phone Number:</td>
+                                </tr>
+                                <tr>
+                                    <td class="info">{{ $student->phone ?? 'N/A' }}</td>
+                                </tr>
+                            </table>
+                        </td>
+                        <td>
+                            <table>
+                                <tr>
+                                    <td class="label">Email:</td>
+                                </tr>
+                                <tr>
+                                    <td class="info">{{ $student->email ?? 'N/A' }}</td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <table>
+                                <tr>
+                                    <td class="label">Address:</td>
+                                </tr>
+                                <tr>
+                                    <td class="info">{{ $student->address ?? 'N/A' }}</td>
+                                </tr>
+                            </table>
+                        </td>
+                        <td>
+                            <table>
+                                <tr>
+                                    <td class="label">CNIC:</td>
+                                </tr>
+                                <tr>
+                                    <td class="info">{{ $student->cnic ?? 'N/A' }}</td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <table>
+                                <tr>
+                                    <td class="label">Education:</td>
+                                </tr>
+                                <tr>
+                                    <td class="info">{{ $student->education ?? 'N/A' }}</td>
+                                </tr>
+                            </table>
+                        </td>
+                        <td>
+                            <table>
+                                <tr>
+                                    <td class="label">Courses:</td>
+                                </tr>
+                                <tr>
+                                    <td class="info">{{ $course[0]->title ?? 'N/A' }}</td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <table>
+                                <tr>
+                                    <td class="label">Admission Date:</td>
+                                </tr>
+                                <tr>
+                                    <td class="info">{{ $student->admission_date ?? 'N/A' }}</td>
+                                </tr>
+                            </table>
+                        </td>
+                        <td>
+                            <table>
+                                <tr>
+                                    <td class="label">Batch:</td>
+                                </tr>
+                                <tr>
+                                    <td class="info">{{ $student->batch->title ?? 'N/A' }}</td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                </table>
+                <div class="footer">
+                    {{-- <p>Date: __________</p> --}}
+                    <br>
+                    <br>
+                    <p>Signature: __________</p>
                 </div>
             </div>
         </div>
     </div>
-</div>
 </body>
+
 </html>
